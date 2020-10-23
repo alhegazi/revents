01788d0b7656ac8e3623f96a9e907cf3954fbda4
Ameen <alhegazi1@gmail.com>
2020-10-23 20:09:37 +0400

    End of section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -4,12 +4,25 @@ import EventDashboard from '../../features/events/eventDashboard/EventDashboard'
 import NavBar from '../../features/nav/NavBar';
 
 function App() {
-  const [formOpen, setFormOpen] = useState(false);
+  const [formOpen, setFormOpen] = useState(true);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
   return (
     <>
-      <NavBar setFormOpen={setFormOpen} />
+      <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className='main'>
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} 
+        selectEvent={handleSelectEvent} selectedEvent={selectedEvent} />
       </Container>
     </>
   );
